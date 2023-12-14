4ff26c286194fa3072074f0f1eeaceb3bb1ab31c
Daria Vdovina <vdovinadn@gmail.com>
2023-12-14 18:57:07 +0300

    feat: string unit-test

--- src/components/string/string-algorithm.ts
+++ src/components/string/string-algorithm.ts
@@ -10,6 +10,9 @@ export const sortArray = async (
     setLoader: (value: React.SetStateAction<boolean>) => void
 ) => {
     const { length } = arr;
+    if (arr.length === 0) {
+        return arr;
+    }
     let start = 0;
     let end;
     let mid = Math.floor(length / 2);
